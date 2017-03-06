18699abdfdcc9a954aeb540d35ca5a23d3428ab5
isotopeee <johnmichael.doroy@gmail.com>
2017-03-05 23:53:04 -0800

    Implement file upload functionality

--- common/models/meal.js
+++ common/models/meal.js
@@ -4,7 +4,6 @@ module.exports = function(Meal) {
   var g = require('strong-globalize')();
   var utils = require('../../node_modules/loopback/lib/utils.js');
 
-  var Dropbox = require('dropbox');
   var path = require('path');
   var formidable = require('formidable');
   var fs = require('fs');
@@ -17,33 +16,36 @@ module.exports = function(Meal) {
 
     var form = new formidable.IncomingForm();
 
-    form.uploadDir = '/temp';
+    form.uploadDir = './uploads/images/meals';
+
+    var new_path = '';
 
     form.on('file', function(name, file) {
-      console.log('File received.');
-      console.log(name);
-      console.log(file);
+      var name = file.name;
+      name = name.slice(0, name.lastIndexOf('.'));
+      name = name + '-' + new Date().toJSON() + '.jpg';
+      name = name.replace(':', '');
+      name = name.replace(':', '');
+      new_path = path.join(form.uploadDir, name);
+
+      fs.rename(file.path, new_path);
     });
 
-    form.on('field', function (name, file) {
+    form.on('field', function (name, value) {
       console.log('Field received.');
-      console.log(name);
-      console.log(file);
     });
 
-    var dbx = new Dropbox({ accessToken: 'zkRM0L0OfnAAAAAAAAAAJ40hAaWmGQlhKpyw-v6S6UncQfxzhQ6sAugQ07zIsGm5'});
     form.parse(req, function (err, fields, files) {
-      dbx.filesUpload({
-        path: '/',
-        contents: fields
-      }).then(function (response) {
-        fn();
-      }).catch(function (err) {
+      if (err) {
+        console.log('Upload failed.');
         var error = new Error(g.f('File upload failed.'));
         error.statusCode = 400;
         error.code = 'FILE_UPLOAD_FAILED';
         return fn(error);
-      });
+      } else {
+        console.log('Upload success.');
+        fn(null, new_path);
+      }
     });
 
     return fn.promise;
@@ -56,6 +58,9 @@ module.exports = function(Meal) {
       description: 'upload meal image',
       accepts: [
         {arg: 'req', type: 'object', http: {source: 'req'}}
+      ],
+      returns: [
+        {arg: 'path', type: 'string'}
       ],
       http: {verb: 'post', path: '/upload', status: '200'}
     }
